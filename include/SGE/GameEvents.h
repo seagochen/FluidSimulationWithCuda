31cb77ccb2e99aea4f099b1061613cc91e33fa69
Orlando Chen <seagochen@gmail.com>
2013-09-09 04:45:23 +0800

    [develop]
    - Analysis

--- include/SGE/GameEvents.h
+++ include/SGE/GameEvents.h
@@ -1,7 +1,7 @@
 /**
 * ------------------------------------the MIT License--------------------------------------------------
 *
-* Copyright (C) <2013> <Orlando Chen>
+* Copyright (C) year 2013 Orlando Chen <seagochen@gmail.com>
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 * associated documentation files (the "Software"), to deal in the Software without restriction, 
 * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
@@ -17,6 +17,7 @@
 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 
 /**
 * <Author>      Orlando Chen
--- include/SGE/SGUtils.h
+++ include/SGE/SGUtils.h
@@ -1,7 +1,7 @@
 /**
 * ------------------------------------the MIT License--------------------------------------------------
 *
-* Copyright (C) <2013> <Orlando Chen>
+* Copyright (C) year 2013 Orlando Chen <seagochen@gmail.com>
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 * associated documentation files (the "Software"), to deal in the Software without restriction, 
 * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
