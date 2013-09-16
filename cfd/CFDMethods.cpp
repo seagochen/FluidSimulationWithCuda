42b6dc7efa73851c134dbb927948da76ef3999af
Orlando Chen <seagochen@gmail.com>
2013-09-16 18:01:39 +0800

    [develop]
    - temp

--- cfd/CFDMethods.cpp
+++ cfd/CFDMethods.cpp
@@ -14,11 +14,11 @@ int CheckBoundary(int u, int v)
 };
 
 
-Vector2d *Velocity2D::SamplePoint(int u, int v)
+Vector2d *Texture2D::SamplePoint(int index_u, int index_v)
 {
-	if (CheckBoundary(u, v) == IN_BOUNDARY)
+	if (CheckBoundary(index_u, index_v) == IN_BOUNDARY)
 	{
-		Vector2d temp(velocity_u[u], velocity_v[v]);
+		Vector2d temp(u[index_u], v[index_v]);
 		return &temp;
 	}
 	else 
@@ -29,7 +29,20 @@ Vector2d *Velocity2D::SamplePoint(int u, int v)
 };
 
 
-Vector2d *Velocity2D::Sample(int Sampling, Vector2d *CellIndex)
+double Texture2D::SampleData(int Sampling, Vector2d *CellIndex)
+{
+	Vector2d temp = *CellIndex;
+	int u = temp[0];
+	int v = temp[1];
+
+	if (Sampling == samPointClamp)
+	{
+		return s[u][v];
+	}
+};
+
+
+Vector2d *Texture2D::Sample(int Sampling, Vector2d *CellIndex)
 {
 	Vector2d temp = *CellIndex;
 	int u = temp[0];
@@ -52,11 +65,61 @@ Vector2d *Velocity2D::Sample(int Sampling, Vector2d *CellIndex)
 	}
 };
 
-Vector2d *advect(double timestep, FLUIDSIM *in, Velocity2D *velocity)
+
+Vector2d *Advect(double timestep, FLUIDSIM *in, Texture2D *velocity)
 {
-	Vector2d OldPos = in->CenterCell;
-	Vector2d CellVelocity = *(velocity->Sample(samPointClamp, &OldPos));
-	Vector2d NewPos = CellVelocity * timestep - OldPos;
+	Vector2d CellVelocity = *(velocity->Sample(samPointClamp, &(in->CenterCell)));
+	Vector2d NewPos = CellVelocity * timestep - in->CenterCell;
 	
 	return velocity->Sample(samLinear, &NewPos);
+};
+
+
+double Divergence(FLUIDSIM *in, Texture2D *velocity)
+{
+	// Get velocity values from neighboring cells
+	Vector2d fieldL = *(velocity->Sample(samPointClamp, &(in->LeftCell)));
+	Vector2d fieldR = *(velocity->Sample(samPointClamp, &(in->RightCell)));
+	Vector2d fieldB = *(velocity->Sample(samPointClamp, &(in->DownCell)));
+	Vector2d fieldU = *(velocity->Sample(samPointClamp, &(in->UpCell)));
+
+	// Compute the velocity's divergence using central differences
+	double divergence = 0.5 * ((fieldR[0] - fieldL[0]) + (fieldU[1] - fieldB[1]));
+
+	return divergence;
+};
+
+
+double Jacobi(FLUIDSIM *in, Texture2D *pressure, Texture2D *divergence)
+{
+	// Get the divergence at the current cell
+	double dC = divergence->SampleData(samPointClamp, &(in->CellIndex));
+
+	// Get pressure values from neighboring cells
+	double pL = pressure->SampleData(samPointClamp, &(in->LeftCell));
+	double pR = pressure->SampleData(samPointClamp, &(in->RightCell));
+	double pU = pressure->SampleData(samPointClamp, &(in->UpCell));
+	double pB = pressure->SampleData(samPointClamp, &(in->DownCell));
+
+	// Compute the new pressure value for the center cell
+	return (pL + pR + pU + pB - dC) / 4.f;
+};
+
+Vector2d *Project(FLUIDSIM *in, Texture2D *pressure, Texture2D *velocity)
+{
+	// Compute the gradient of pressure at the current cell by taking central differences 
+	// of neighboring pressure values
+	double pL = pressure->SampleData(samPointClamp, &(in->LeftCell));
+	double pR = pressure->SampleData(samPointClamp, &(in->RightCell));
+	double pU = pressure->SampleData(samPointClamp, &(in->UpCell));
+	double pB = pressure->SampleData(samPointClamp, &(in->DownCell));
+
+	Vector2d gradP(pR - pL, pU - pB);
+	gradP *= 0.5f;
+
+	// Project the velocity onto its divergence-free component by subtracting the gradient of pressure
+	Vector2d vOld = *velocity->Sample(samPointClamp, &(in->CellIndex));
+	Vector2d vNew = vOld - gradP;
+
+	return &vNew;
 };
--- cfd/CFDMethods.h
+++ cfd/CFDMethods.h
@@ -26,15 +26,21 @@ namespace sge
 		Vector2d DownCell;
 	};
 	
-	/* 定義二維速度場 */
-	class Velocity2D
+	/* 2D 紋理數據定義 */
+	class Texture2D
 	{
 	public:
-		double velocity_u[SIMAREA_WIDTH];
-		double velocity_v[SIMAREA_HEIGHT];
+		// Vector fields
+		double u[SIMAREA_WIDTH];
+		double v[SIMAREA_HEIGHT];
+		// Scalar fields
+		double s[SIMAREA_WIDTH][SIMAREA_HEIGHT];
 
 	public:
+		// Return a vector
 		Vector2d *Sample(int Sampling, Vector2d *CellIndex);
+		// Return a scalar
+		double SampleData(int Sampling, Vector2d *CellIndex);
 
 	private:
 		Vector2d *SamplePoint(int u, int v);
@@ -43,7 +49,16 @@ namespace sge
 
 	// 由二維速度場計算出經過時間timestep後，由速度平流影響到的新格點位置
 	// 並使用Linear filtering對新坐標賦值
-	Vector2d *advect(double timestep, FLUIDSIM *in, Velocity2D *velocity);
+	Vector2d *Advect(double timestep, FLUIDSIM *in, Texture2D *velocity);
+
+	// 計算散度
+	double Divergence(FLUIDSIM *in, Texture2D *velocity);
+
+	// Jacobi
+	double Jacobi(FLUIDSIM *in, Texture2D *pressure, Texture2D *divergence);
+
+	// 將advect divergence 及 Jacobi 等計算出的值用於計算結果 dt 後產生的新的速度場
+	Vector2d *Project(FLUIDSIM *in, Texture2D *pressure, Texture2D *velocity);
 
 };
 
