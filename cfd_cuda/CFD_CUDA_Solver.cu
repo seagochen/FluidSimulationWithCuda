3f83dedc6693a22260f6764939e749b1d9595d01
Orlando Chen <seagochen@gmail.com>
2013-10-11 00:10:32 +0800

    Deleted unnecessary files

--- cfd_cuda/CFD_CUDA_Solver.cu
+++ cfd_cuda/CFD_CUDA_Solver.cu
@@ -17,16 +17,16 @@ __global__ void add_source_kernel ( float * grid_out, float * src_in, float dt_i
 	grid_out[i] += dt_in*src_in[i];
 }
 
-
-__global__ void set_bnd_kernel ( int GridSize, int boundary, float * grid )
+void set_bnd ( int GridSize, int boundary, float * grid )
 {
-	int i = threadIdx.x;
+	int i;
 
-	grid[IX(0  ,i)] = boundary==1 ? -grid[IX(1,i)] : grid[IX(1,i)];
-	grid[IX(GridSize+1,i)] = boundary==1 ? -grid[IX(GridSize,i)] : grid[IX(GridSize,i)];
-	grid[IX(i,0  )] = boundary==2 ? -grid[IX(i,1)] : grid[IX(i,1)];
-	grid[IX(i,GridSize+1)] = boundary==2 ? -grid[IX(i,GridSize)] : grid[IX(i,GridSize)];
-
+	for ( i=1 ; i<=GridSize ; i++ ) {
+		grid[IX(0  ,i)] = boundary==1 ? -grid[IX(1,i)] : grid[IX(1,i)];
+		grid[IX(GridSize+1,i)] = boundary==1 ? -grid[IX(GridSize,i)] : grid[IX(GridSize,i)];
+		grid[IX(i,0  )] = boundary==2 ? -grid[IX(i,1)] : grid[IX(i,1)];
+		grid[IX(i,GridSize+1)] = boundary==2 ? -grid[IX(i,GridSize)] : grid[IX(i,GridSize)];
+	}
 	grid[IX(0  ,0  )] = 0.5f*(grid[IX(1,0  )]+grid[IX(0  ,1)]);
 	grid[IX(0  ,GridSize+1)] = 0.5f*(grid[IX(1,GridSize+1)]+grid[IX(0  ,GridSize)]);
 	grid[IX(GridSize+1,0  )] = 0.5f*(grid[IX(GridSize,0  )]+grid[IX(GridSize+1,1)]);
@@ -34,16 +34,21 @@ __global__ void set_bnd_kernel ( int GridSize, int boundary, float * grid )
 }
 
 
-__global__ void lin_solve_kernel ( int GridSize, int boundary, float * grid, float * grid0, float a, float c )
+void lin_solve ( int GridSize, int boundary, float * grid, float * grid0, float a, float c )
 {
-	int index_x = blockIdx.x * blockDim.x + threadIdx.x;
-	int index_y = blockIdx.y * blockDim.y + threadIdx.y;
-	
-	// map the two 2D indices to a single linear, 1D index
-	int grid_width = gridDim.x * blockDim.x;
-	int index = index_y * grid_width + index_x;
+	int i, j, k;
 
-	grid[index] = (grid0[index] + a*(grid[IX(i-1,j)]+grid[IX(i+1,j)]+grid[IX(i,j-1)]+grid[IX(i,j+1)]))/c;
+	for ( k=0 ; k<20 ; k++ ) 
+	{
+		for ( i=1 ; i<=GridSize ; i++ )
+		{
+			for ( j=1 ; j<=GridSize ; j++ ) 
+			{
+				grid[IX(i,j)] = (grid0[IX(i,j)] + a*(grid[IX(i-1,j)]+grid[IX(i+1,j)]+grid[IX(i,j-1)]+grid[IX(i,j+1)]))/c;
+			}
+		}
+		set_bnd ( GridSize, boundary, grid );
+	}
 }
 
 
@@ -53,6 +58,55 @@ void diffuse ( int GridSize, int boundary, float * grid, float * grid0, float di
 	lin_solve ( GridSize, boundary, grid, grid0, a, 1+4*a );
 }
 
+
+void advect ( int GridSize, int boundary, float * density, float * density0, float * u, float * v, float dt )
+{
+	int i, j, i0, j0, i1, j1;
+	float grid, y, s0, t0, s1, t1, dt0;
+
+	dt0 = dt*GridSize;
+	for ( i=1 ; i<=GridSize ; i++ ) 
+	{
+		for ( j=1 ; j<=GridSize ; j++ ) 
+		{
+			grid = i-dt0*u[IX(i,j)]; y = j-dt0*v[IX(i,j)];
+			if (grid<0.5f) grid=0.5f; if (grid>GridSize+0.5f) grid=GridSize+0.5f; i0=(int)grid; i1=i0+1;
+			if (y<0.5f) y=0.5f; if (y>GridSize+0.5f) y=GridSize+0.5f; j0=(int)y; j1=j0+1;
+			s1 = grid-i0; s0 = 1-s1; t1 = y-j0; t0 = 1-t1;
+			density[IX(i,j)] = s0*(t0*density0[IX(i0,j0)]+t1*density0[IX(i0,j1)])+
+				s1*(t0*density0[IX(i1,j0)]+t1*density0[IX(i1,j1)]);
+		}
+	}
+	set_bnd ( GridSize, boundary, density );
+}
+
+
+void project ( int GridSize, float * u, float * v, float * p, float * div )
+{
+	int i, j;
+
+	for ( i=1 ; i<=GridSize ; i++ )
+	{
+		for ( j=1 ; j<=GridSize ; j++ )
+		{
+			div[IX(i,j)] = -0.5f*(u[IX(i+1,j)]-u[IX(i-1,j)]+v[IX(i,j+1)]-v[IX(i,j-1)])/GridSize;		
+			p[IX(i,j)] = 0;
+		}
+	}	
+	set_bnd ( GridSize, 0, div ); set_bnd ( GridSize, 0, p );
+
+	lin_solve ( GridSize, 0, p, div, 1, 4 );
+
+	for ( i=1 ; i<=GridSize ; i++ )
+	{
+		for ( j=1 ; j<=GridSize ; j++ ) 
+		{
+			u[IX(i,j)] -= 0.5f*GridSize*(p[IX(i+1,j)]-p[IX(i-1,j)]);
+			v[IX(i,j)] -= 0.5f*GridSize*(p[IX(i,j+1)]-p[IX(i,j-1)]);
+		}
+	}
+	set_bnd ( GridSize, 1, u ); set_bnd ( GridSize, 2, v );
+}
 
 cudaError_t cuda_dens_step( int GridSize, float *grid, float *grid0, float *u, float *v, float diff, float dt )
 {
@@ -64,16 +118,6 @@ cudaError_t cuda_dens_step( int GridSize, float *grid, float *grid0, float *u, f
 	int size = (GridSize+2)*(GridSize+2);
 
 	cudaError_t cuda_status;
-
-	// create two dimensional 4x4 thread blocks
-	dim3 block_size;
-	block_size.x = 64 + 2;
-	block_size.y = 64 + 2;
-	
-	// configure a two dimensional grid as well
-	dim3 grid_size;
-	grid_size.x = 66 / block_size.x;
-	grid_size.y = 66 / block_size.y;
 
 	// Choose which GPU to run on, change this on a multi-GPU system
 	cuda_status = cudaSetDevice(0);
@@ -153,27 +197,25 @@ cudaError_t cuda_dens_step( int GridSize, float *grid, float *grid0, float *u, f
 		return cuda_status;
 	}
 
-	SWAP ( grid0, grid );
+	SWAP ( grid0, grid ); diffuse ( GridSize, 0, grid, grid0, diff, dt );
+	SWAP ( grid0, grid ); advect ( GridSize, 0, grid, grid0, u, v, dt );
 
 	return cuda_status;
 };
 
+void add_source ( int GridSize, float * grid, float * src, float dt )
+{
+	int i, size=(GridSize+2)*(GridSize+2);
+	for ( i=0 ; i<size ; i++ ) grid[i] += dt*src[i];
+}
+
 cudaError_t cuda_vel_step( int GridSize, float * u, float * v, float * u0, float * v0, float visc, float dt )
 {
-	float *u_cuda = 0;
-	float *v_cuda = 0;
-	float *u0_cuda= 0;
-	float *v0_cuda= 0;
-
-	cudaError cuda_status;
-
-	// Choose which GPU to run on, change this on a multi-GPU system
-	cuda_status = cudaSetDevice(0);
-	if ( cuda_status != cudaError::cudaSuccess ) {
-		ErrorMSG("cudaSetDevice failed! Do you have a CUDA-capable GPU installed?");
-		CUDA_Release(u_cuda);
-		CUDA_Release(v_cuda);
-		CUDA_Release(u0_cuda);
-		CUDA_Release(v0_cuda);
-	}
+	add_source ( GridSize, u, u0, dt ); add_source ( GridSize, v, v0, dt );
+	SWAP ( u0, u ); diffuse ( GridSize, 1, u, u0, visc, dt );
+	SWAP ( v0, v ); diffuse ( GridSize, 2, v, v0, visc, dt );
+	project ( GridSize, u, v, u0, v0 );
+	SWAP ( u0, u ); SWAP ( v0, v );
+	advect ( GridSize, 1, u, u0, u0, v0, dt ); advect ( GridSize, 2, v, v0, u0, v0, dt );
+	project ( GridSize, u, v, u0, v0 );
 };
