974c01b4a6d38a923890dee487a6221e38d4185e
Orlando Chen <seagochen@gmail.com>
2013-12-14 00:13:13 +0800

    prepared the stage for glsl

--- ide/main.cpp
+++ ide/main.cpp
@@ -1,7 +1,7 @@
 #ifndef __main_cpp_
 #define __main_cpp_
 
-#include "glew32.h"
+#include <GL\glew.h>
 #include <GL\freeglut.h>
 
 //#include <SGE\SGUtils.h>
@@ -11,35 +11,21 @@
 
 #include <SGE\SGUtils.h>
 
+using namespace sge;
+using namespace std;
 
 void onCreate ()
 {
-	using namespace sge;
 
-	GLenum err = glewInit();
-	if ( GLEW_OK != err )
-	{
-		/* Problem: glewInit failed, something is seriously wrong. */
-		FileManager::Write ( "runtime.log", SG_APPEND, 
-			"glewInit failed: %s", glewGetErrorString(err));
-		exit (1);
-	}
-	else
-	{
-		FileManager::Write ( "runtime.log", SG_APPEND, "glewInit successed" );
-	}
 
-	std::string str;
-	char *msg = 0;
-	FileManager::Read ( ".\\Shader\\simple.vert", &str );
-	FileManager::Read ( ".\\Shader\\simple.vert", 10000, &msg);
+	goto Success;
 
-	using namespace std;
+Error:
+	system ("pause");
+	exit (1);
 
-	cout<<"----------"<<endl;
-	cout<<str<<endl<<endl;
-	cout<<"----------"<<endl;
-	cout<<msg<<endl;
+Success:
+	;
 };
 
 
