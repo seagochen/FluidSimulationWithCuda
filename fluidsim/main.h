716e149d9a742f19a03c8ffdb781ead26e723552
Orlando Chen <seagochen@gmail.com>
2013-12-17 00:03:18 +0800

    volume rendering sample code by using GLSL

--- fluidsim/main.h
+++ fluidsim/main.h
@@ -13,6 +13,9 @@
 using namespace sge;
 using namespace std;
 
+#define BACKFACE_SIZE_X  400
+
+
 // Initialize 'shader helper' class, and set the 'handles' of OpenGL program and shader objects
 extern void CreateShaders 
 	( Shader *shader_out, GLuint *prog_out, 
@@ -21,7 +24,21 @@ extern void CreateShaders
 // Sets 1-D texture for transfer function
 extern GLuint Create1DTransFunc ( void );
 
+// Sets 2-D texture for backface
+extern GLuint Create2DBackFace ( void );
+
 // Sets 3-D texture for volumetric data 
-extern GLuint Create3DVolumFunc ( void );
+extern GLuint Create3DVolumetric ( void );
+
+// Sets framebuffer
+extern GLuint CreateFrameBuffer ( GLuint texObj );
+
+// Sets rendering face
+extern void RenderingFace ( GLenum cullFace, GLfloat angle, GLuint program, GLuint cluster );
+
+extern void SetVolumeInfoUinforms ( GLuint program, GLuint Tex1DTrans, GLuint Tex2DBF, GLuint Tex3DVol, 
+							GLfloat width, GLfloat height, GLfloat stepsize );
+
+extern GLuint InitVerticesBufferObj ( void );
 
 #endif
