83778eea4f0465ce6089556d62b5cbf87292248e
Orlando Chen <seagochen@gmail.com>
2014-01-11 15:40:11 +0800

    new idea before implementation

---
+++ fluidsim_dynamic/backup/OperationsDynamic.h
@@ -0,0 +1,57 @@
+/**
+* <Author>      Orlando Chen
+* <First>       Jan 09, 2014
+* <Last>		Jan 09, 2014
+* <File>        OperationDynamic.h
+*/
+
+#ifndef __operation_dynamic_h_
+#define __operation_dynamic_h_
+
+#include "FluidSimAreaDynamic.h"
+#include "FluidMathLibDynamic.h"
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+__global__ void kernelPickData ( unsigned char *data, double const *grid )
+{
+	GetIndex();
+
+	// Zero data first
+	data [ Index (i, j, k) ] = 0;
+
+	// retrieve data from grid
+	int temp = sground ( grid[ Index(i,j,k)] );
+	if ( temp > 0 and temp < 250 )
+		data [ Index(i, j, k) ] = (unsigned char) temp;
+};
+
+__global__ void kernelCopyBuffer ( double *grid_out, double const *grid_in )
+{
+	GetIndex ();
+
+	grid_out [ Index(i,j,k) ] = grid_in [ Index(i, j, k) ];
+};
+
+__global__ void kernelSwapBuffer ( double *grid1, double *grid2 )
+{
+	GetIndex ();
+
+	double temp = grid1 [ Index(i,j,k) ];
+	grid1 [ Index(i,j,k) ] = grid2 [ Index(i,j,k) ];
+	grid2 [ Index(i,j,k) ] = temp;
+};
+
+__global__ void kernelZeroBuffer ( double *grid )
+{
+	GetIndex ();
+	grid [ Index(i,j,k) ] = 0.f;
+};
+
+__host__ void hostSwapBuffer ( double *grid1, double *grid2 )
+{
+	cudaDeviceDim3D();
+	kernelSwapBuffer cudaDevice(gridDim, blockDim) (grid1, grid2);
+};
+
+#endif
