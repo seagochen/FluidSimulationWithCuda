0fc3057db67c4f09747630258d946440d1546a9e
Orlando Chen <seagochen@gmail.com>
2014-03-31 00:18:40 +0800

    <develop> x256粗計算正確顯示，接下來需要做精細計算.

--- Refinement_x256/FluidSimProc.cu
+++ Refinement_x256/FluidSimProc.cu
@@ -197,10 +197,10 @@ void FluidSimProc::InitBoundary( void )
 	m_scHelper.DeviceParamDim( &gridDim, &blockDim, THREADS_S,
 		TILE_X, TILE_Y, gGRIDS_X, gGRIDS_Y, gGRIDS_Z );
 
-	kernelSetBound __device_func__ ( gGrids_obstacle, gGRIDS_X, gGRIDS_Y, gGRIDS_Z );
+	kernelSetBound __device_func__ ( m_vectgGrids[DEV_OBSTACLE], gGRIDS_X, gGRIDS_Y, gGRIDS_Z );
 
 	kernelLoadBullet __device_func__
-		( gBullets_obstacle, gGrids_obstacle,
+		( m_vectgBullets[DEV_OBSTACLE], m_vectgGrids[DEV_OBSTACLE],
 		gBULLET_X, gBULLET_Y, gBULLET_Z, gGRIDS_X, gGRIDS_Y, gGRIDS_Z );
 
 	if ( m_scHelper.GetCUDALastError( "call member function InitBoundary failed", __FILE__, __LINE__ ) )
@@ -213,24 +213,24 @@ void FluidSimProc::InitBoundary( void )
 
 void FluidSimProc::GenerVolumeImg( void )
 {
-	m_scHelper.DeviceParamDim( &gridDim, &blockDim, THREADS_S,
-		gGRIDS_X / 2, gGRIDS_Y / 2, gGRIDS_X, gGRIDS_Y, gGRIDS_Z );
+	m_scHelper.DeviceParamDim( &gridDim, &blockDim, THREADS_S, 32, 32, 64, 64, 64 );
 
-	kernelExitBullet __device_func__ ( gGrids_density, gBullets_density, 
-		gGRIDS_X, gGRIDS_Y, gGRIDS_Z,
-		gBULLET_X, gBULLET_Y, gBULLET_Z );
+	kernelExitBullet __device_func__ ( m_vectgGrids[DEV_DENSITY], m_vectgBullets[DEV_DENSITY], 
+		64, 64, 64,  66, 66, 66 );
 
+	m_scHelper.DeviceParamDim( &gridDim, &blockDim, THREADS_S, 32, 32, 256, 256, 256 );
 
-	m_scHelper.DeviceParamDim( &gridDim, &blockDim, THREADS_S,
-		TILE_X, TILE_Y, BIG_X, BIG_Y, BIG_Z );
+	kernelUpScalingInterpolation __device_func__
+		( m_vectBigBuffers[DEV_DENSITY], m_vectgGrids[DEV_DENSITY],
+		64, 64, 64, 256, 256, 256, 4, 4, 4 );
 
-	kernelUpScalingInterpolation __device_func__ ( Big_density, gGrids_density,
-		gGRIDS_X, gGRIDS_Y, gGRIDS_Z,
-		BIG_X, BIG_Y, BIG_Z,
-		4, 4, 4 );
+	kernelPickData __device_func__ ( m_ptrDeviceVisual, m_vectBigBuffers[DEV_DENSITY],
+		VOLUME_X, VOLUME_Y, VOLUME_Z );
 
-	kernelPickData __device_func__ ( m_ptrDeviceVisual, Big_density, VOLUME_X, VOLUME_Y, VOLUME_Z );
-
+//	kernelPickData __device_func__ ( m_ptrDeviceVisual, m_vectgGrids[DEV_DENSITY], 
+//		VOLUME_X, VOLUME_Y, VOLUME_Z,
+//		64, 64, 64,
+//		0, 0, 0 );
 
 	if ( m_scHelper.GetCUDALastError( "call member function GenerVolumeImg failed", __FILE__, __LINE__ ) )
 	{
@@ -239,28 +239,42 @@ void FluidSimProc::GenerVolumeImg( void )
 	}
 };
 
+#if 0
+#define dev_den m_vectgBullets[DEV_DENSITY]
+#define	dev_u   m_vectgBullets[DEV_VELOCITY_U]
+#define	dev_v   m_vectgBullets[DEV_VELOCITY_V]
+#define	dev_w   m_vectgBullets[DEV_VELOCITY_W]
+#define	dev_p   m_vectgBullets[DEV_PRESSURE]
+#define	dev_div m_vectgBullets[DEV_DIVERGENCE]
+#define	dev_obs m_vectgBullets[DEV_OBSTACLE]
+#define	dev_den0 m_vectgBullets[DEV_DENSITY0]
+#define	dev_u0   m_vectgBullets[DEV_VELOCITY_U0]
+#define	dev_v0   m_vectgBullets[DEV_VELOCITY_V0]
+#define	dev_w0   m_vectgBullets[DEV_VELOCITY_W0]
+#endif
 
 void FluidSimProc::SolveGlobalFlux()
 {
-	dev_den  = gBullets_density;
-	dev_u    = gBullets_velocity_u;
-	dev_v    = gBullets_velocity_v;
-	dev_w    = gBullets_velocity_w;
-	dev_p    = gBullets_pressure;
-	dev_div  = gBullets_divergence;
-	dev_obs  = gBullets_obstacle;
-	dev_den0 = gBullets_density0;
-	dev_u0   = gBullets_velocity_u0;
-	dev_v0   = gBullets_velocity_v0;
-	dev_w0   = gBullets_velocity_w0;
+#if 1
+	dev_den  = &m_vectgBullets[DEV_DENSITY];
+	dev_u    = &m_vectgBullets[DEV_VELOCITY_U];
+	dev_v    = &m_vectgBullets[DEV_VELOCITY_V];
+	dev_w    = &m_vectgBullets[DEV_VELOCITY_W];
+	dev_p    = &m_vectgBullets[DEV_PRESSURE];
+	dev_div  = &m_vectgBullets[DEV_DIVERGENCE];
+	dev_obs  = &m_vectgBullets[DEV_OBSTACLE];
+	dev_den0 = &m_vectgBullets[DEV_DENSITY0];
+	dev_u0   = &m_vectgBullets[DEV_VELOCITY_U0];
+	dev_v0   = &m_vectgBullets[DEV_VELOCITY_V0];
+	dev_w0   = &m_vectgBullets[DEV_VELOCITY_W0];
 
-	//TODO
 
 	if ( m_scHelper.GetCUDALastError( "call member function SolveGlobalFlux failed", __FILE__, __LINE__ ) )
 	{
 		FreeResource();
 		exit(1);
 	}
+#endif
 };
 
 
@@ -270,7 +284,8 @@ void FluidSimProc::FluidSimSolver( FLUIDSPARAM *fluid )
 
 	SolveGlobalFlux();
 
-	SolveNavierStokesEquation( DELTATIME, true, true, true, gBULLET_X, gBULLET_Y, gBULLET_Z );
+	SolveNavierStokesEquation( DELTATIME, true, true, true,
+		32, 32, gGRIDS_X, gGRIDS_Y, gGRIDS_Z, gBULLET_X, gBULLET_Y, gBULLET_Z );
 	
 	GenerVolumeImg();
 
@@ -279,11 +294,162 @@ void FluidSimProc::FluidSimSolver( FLUIDSPARAM *fluid )
 
 
 void FluidSimProc::SolveNavierStokesEquation
-	( cdouble dt, bool add, bool vel, bool den, cint bx, cint by, cint bz )
+			( cdouble dt, bool add, bool vel, bool den,
+			cint tx, cint ty,
+			cint gx, cint gy, cint gz,
+			cint bx, cint by, cint bz )
 {
+	m_scHelper.DeviceParamDim( &gridDim, &blockDim, THREADS_S, tx, ty, gx, gy, gz );
+
+#if 0
+	double *ptr;
+	m_scHelper.CreateHostBuffers( gBULLET_X * gBULLET_Y * gBULLET_Z * sizeof(double), 1, &ptr );
+
+//	cudaMemcpy( ptr, dev_den, gBULLET_X * gBULLET_Y * gBULLET_Z * sizeof(double), cudaMemcpyDeviceToHost );
+//
+//	for ( int j = 0; j < gBULLET_Y; j++ )
+//	{
+//		for ( int k = 0; k < gBULLET_Z; k++ )
+//		{
+//			for ( int i = 0; i < gBULLET_X; i++ )
+//			{
+//				printf( "%d ", (int)ptr[ix(i,j,k,gBULLET_X,gBULLET_Y)] );
+//			}
+//			printf("\n");
+//		}
+//		printf("\n");
+//	}
+
+	cout << "----------------------------------------------------------------------" << endl;
+
+#endif
+
 	if (add) SourceSolver( dt, bx, by, bz );
 	if (vel) VelocitySolver( dt, bx, by, bz );
 	if (den) DensitySolver( dt, bx, by, bz );
+
+
+#if 0
+
+	cudaMemcpy( ptr, dev_p, gBULLET_X * gBULLET_Y * gBULLET_Z * sizeof(double), cudaMemcpyDeviceToHost );
+
+	for ( int j = 0; j < gBULLET_Y; j++ )
+	{
+		for ( int k = 0; k < gBULLET_Z; k++ )
+		{
+			for ( int i = 0; i < gBULLET_X; i++ )
+			{
+				printf( "%f ", ptr[ix(i,j,k,gBULLET_X,gBULLET_Y)] );
+			}
+			printf("\n");
+		}
+		printf("\n");
+	}
+
+	free(ptr);
+	FreeResource();
+	exit(0);
+
+#endif
 };
 
-//TODO
+
+void FluidSimProc::SourceSolver( cdouble dt, cint bx, cint by, cint bz )
+{
+	kernelAddSource __device_func__
+		( *dev_den, *dev_v, bx, by, bz, 
+			*dev_obs, dt, (double)(rand() % 300 + 1) / 100.f );
+};
+
+
+void FluidSimProc::VelocitySolver( cdouble dt, cint bx, cint by, cint bz )
+{
+	// diffuse the velocity field (per axis):
+	Diffusion( *dev_u0, *dev_u, VISOCITY, bx, by, bz );
+	Diffusion( *dev_v0, *dev_v, VISOCITY, bx, by, bz );
+	Diffusion( *dev_w0, *dev_w, VISOCITY, bx, by, bz );
+	
+	if ( m_scHelper.GetCUDALastError( "host function failed: Diffusion", __FILE__, __LINE__ ) )
+	{
+		FreeResource();
+		exit( 1 );
+	}
+
+	std::swap( *dev_u0, *dev_u );
+	std::swap( *dev_v0, *dev_v );
+	std::swap( *dev_w0, *dev_w );
+
+	// stabilize it: (vx0, vy0 are whatever, being used as temporaries to store gradient field)
+	Projection( *dev_u, *dev_v, *dev_w, *dev_div, *dev_p, bx, by, bz );
+
+	if ( m_scHelper.GetCUDALastError( "host function failed: Projection", __FILE__, __LINE__ ) )
+	{
+		FreeResource();
+		exit( 1 );
+	}
+	
+	// advect the velocity field (per axis):
+	Advection( *dev_u0, *dev_u, *dev_u, *dev_v, *dev_w, dt, bx, by, bz );
+	Advection( *dev_v0, *dev_v, *dev_u, *dev_v, *dev_w, dt, bx, by, bz );
+	Advection( *dev_w0, *dev_w, *dev_u, *dev_v, *dev_w, dt, bx, by, bz );
+
+	if ( m_scHelper.GetCUDALastError( "host function failed: Advection", __FILE__, __LINE__ ) )
+	{
+		FreeResource();
+		exit( 1 );
+	}
+
+	std::swap( *dev_u0, *dev_u );
+	std::swap( *dev_v0, *dev_v );
+	std::swap( *dev_w0, *dev_w );
+	
+	// stabilize it: (vx0, vy0 are whatever, being used as temporaries to store gradient field)
+	Projection( *dev_u, *dev_v, *dev_w, *dev_div, *dev_p, bx, by, bz );
+};
+
+
+void FluidSimProc::DensitySolver( cdouble dt, cint bx, cint by, cint bz )
+{
+	Diffusion( *dev_den0, *dev_den, DIFFUSION, bx, by, bz );
+	std::swap( *dev_den0, *dev_den );
+	Advection( *dev_den, *dev_den0, *dev_u, *dev_v, *dev_w, dt, bx, by, bz );
+
+	if ( m_scHelper.GetCUDALastError( "host function failed: DensitySolver", __FILE__, __LINE__ ) )
+	{
+		FreeResource();
+		exit( 1 );
+	}
+};
+
+
+void FluidSimProc::Jacobi
+	( double *out, cdouble *in, cdouble diff, cdouble divisor, cint bx, cint by, cint bz )
+{
+	for ( int k=0; k<20; k++)
+		kernelJacobi __device_func__ ( out, in, bx, by, bz, diff, divisor );
+};
+
+void FluidSimProc::Advection
+	( double *out, cdouble *in, cdouble *u, cdouble *v, cdouble *w, cdouble dt, cint bx, cint by, cint bz )
+{	
+	kernelAdvection __device_func__ ( out, in, bx, by, bz, dt, u, v, w );
+};
+
+void FluidSimProc::Diffusion( double *out, cdouble *in, cdouble diff, cint bx, cint by, cint bz )
+{
+	double rate = diff * bx * by * bz;
+	Jacobi( out, in, rate, 1+6*rate, bx, by, bz );
+};
+
+void FluidSimProc::Projection
+	( double *u, double *v, double *w, double *div, double *p, cint bx, cint by, cint bz )
+{
+	// the velocity gradient
+	kernelGradient __device_func__ ( div, p, bx, by, bz, u, v, w );
+
+	// reuse the Gauss-Seidel relaxation solver to safely diffuse the velocity gradients from p to div
+	Jacobi(p, div, 1.f, 6.f, bx, by, bz);
+
+	// now subtract this gradient from our current velocity field
+	kernelSubtract __device_func__ ( u, v, w, p, bx, by, bz );
+};
--- Refinement_x256/FluidSimProc.h
+++ Refinement_x256/FluidSimProc.h
@@ -25,52 +25,23 @@ namespace sge
 {	
 #define __device_func__ <<<gridDim,blockDim>>>
 
-#define gGrids_density       m_vectgGrids[0]
-#define gGrids_velocity_u    m_vectgGrids[1]
-#define gGrids_velocity_v    m_vectgGrids[2]
-#define gGrids_velocity_w    m_vectgGrids[3]
-#define gGrids_obstacle      m_vectgGrids[4]
-
-#define gBullets_density     m_vectgBullets[0]
-#define gBullets_velocity_u  m_vectgBullets[1]
-#define gBullets_velocity_v  m_vectgBullets[2]
-#define gBullets_velocity_w  m_vectgBullets[3]
-#define gBullets_obstacle    m_vectgBullets[4]
-#define gBullets_density0    m_vectgBullets[5]
-#define gBullets_velocity_u0 m_vectgBullets[6]
-#define gBullets_velocity_v0 m_vectgBullets[7]
-#define gBullets_velocity_w0 m_vectgBullets[8]
-#define gBullets_divergence  m_vectgBullets[9]
-#define gBullets_pressure    m_vectgBullets[10]
-	
-#define Big_density          m_vectBigBuffers[0]
-#define Big_velocity_u       m_vectBigBuffers[1]
-#define Big_velocity_v       m_vectBigBuffers[2]
-#define Big_velocity_w       m_vectBigBuffers[3]
-#define Big_obstacle         m_vectBigBuffers[4]
-
-#define sGrids_density       m_vectsGrids[0]
-#define sGrids_velocity_u    m_vectsGrids[1]
-#define sGrids_velocity_v    m_vectsGrids[2]
-#define sGrids_velocity_w    m_vectsGrids[3]
-#define sGrids_obstacle      m_vectsGrids[4]
-
-#define sBullets_density     m_vectsBullets[0]
-#define sBullets_velocity_u  m_vectsBullets[1]
-#define sBullets_velocity_v  m_vectsBullets[2]
-#define sBullets_velocity_w  m_vectsBullets[3]
-#define sBullets_obstacle    m_vectsBullets[4]
-#define sBullets_density0    m_vectsBullets[5]
-#define sBullets_velocity_u0 m_vectsBullets[6]
-#define sBullets_velocity_v0 m_vectsBullets[7]
-#define sBullets_velocity_w0 m_vectsBullets[8]
-#define sBullets_divergence  m_vectsBullets[9]
-#define sBullets_pressure    m_vectsBullets[10]
+#define DEV_DENSITY     0
+#define DEV_VELOCITY_U  1
+#define DEV_VELOCITY_V  2
+#define DEV_VELOCITY_W  3
+#define DEV_OBSTACLE    4
+#define DEV_DIVERGENCE  5
+#define DEV_PRESSURE    6
+#define DEV_DENSITY0    7
+#define DEV_VELOCITY_U0 8
+#define DEV_VELOCITY_V0 9
+#define DEV_VELOCITY_W0 10
 
 	class FluidSimProc
 	{
 	private:
-		pdouble dev_den, dev_u, dev_v, dev_w, dev_p, dev_div, dev_obs, dev_den0, dev_u0, dev_v0, dev_w0;
+		double **dev_den, **dev_u, **dev_v, **dev_w, **dev_p, **dev_div, **dev_obs,
+			**dev_den0, **dev_u0, **dev_v0, **dev_w0;
 
 		vector<double*> m_vectgGrids, m_vectgBullets;
 		vector<double*> m_vectsGrids, m_vectsBullets;
@@ -115,21 +86,32 @@ namespace sge
 		void SolveGlobalFlux( void );
 
 	private:
-		void SolveNavierStokesEquation( cdouble dt, bool add, bool vel, bool dens, cint bx, cint by, cint bz );
+		void SolveNavierStokesEquation
+			( cdouble dt, bool add, bool vel, bool dens,
+			cint tx, cint ty,
+			cint gx, cint gy, cint gz,
+			cint bx, cint by, cint bz );
 
-		void DensitySolver( cdouble dt, cint bx, cint by, cint bz );
+		void DensitySolver( cdouble dt, 
+			cint bx, cint by, cint bz );
 
-		void VelocitySolver( cdouble dt, cint bx, cint by, cint bz );
+		void VelocitySolver( cdouble dt,
+			cint bx, cint by, cint bz );
 
-		void SourceSolver( cdouble dt, cint bx, cint by, cint bz );
+		void SourceSolver( cdouble dt,
+			cint bx, cint by, cint bz );
 
-		void Jacobi( double *out, cdouble *in, cdouble diff, cdouble divisor, cint bx, cint by, cint bz );
+		void Jacobi( double *out, cdouble *in, cdouble diff, cdouble divisor,
+			cint bx, cint by, cint bz );
 
-		void Advection( double *out, cdouble *in, cdouble *u, cdouble *v, cdouble *w, cdouble dt, cint bx, cint by, cint bz );
+		void Advection( double *out, cdouble *in, cdouble *u, cdouble *v, cdouble *w, cdouble dt,
+			cint bx, cint by, cint bz );
 
-		void Diffusion( double *out, cdouble *in, cdouble diff, cint bx, cint by, cint bz );
+		void Diffusion( double *out, cdouble *in, cdouble diff,
+			cint bx, cint by, cint bz );
 
-		void Projection( double *u, double *v, double *w, double *div, double *p, cint bx, cint by, cint bz );
+		void Projection( double *u, double *v, double *w, double *div, double *p,
+			cint bx, cint by, cint bz );
 	};
 };
 
